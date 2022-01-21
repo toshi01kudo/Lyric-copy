034eec7e7f0cc2e4bd93f291fd542adeb8cba4a3
toshi01kudo <72183025+toshi01kudo@users.noreply.github.com>
2022-01-22 00:28:29 +0900

    Create Lyric-copy.user.js

---
+++ Lyric-copy.user.js
@@ -0,0 +1,40 @@
+// ==UserScript==
+// @name         Lyric copy
+// @namespace    Lyric copy
+// @version      0.1
+// @description  copy the lyric
+// @author       Toshihito Kudo
+// @include      https://www.uta-net.com/song*
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // ボタン生成
+    $(".kashi-related").after(`<div id="js-copybtn" class="col-3"><a class="btn btn-sm btn-light w-100 px-0">Copy Lyric</a></div>`);
+    $('#js-copybtn').click(function(){
+        // コピーする文章の取得
+        let lyric = $('#kashi_area').text();
+        // テキストエリアの作成
+        let clipboard = $('<textarea></textarea>');
+        // テキストエリアに文章を挿入
+        clipboard.text(lyric);
+        //　テキストエリアを挿入
+        $('body').append(clipboard);
+        //　テキストエリアを選択
+        clipboard.select();
+        // コピー
+        navigator.clipboard.writeText(lyric)
+            .then(() => {
+            console.log("Text copied to clipboard...")
+        })
+            .catch(err => {
+            console.log('Something went wrong', err);
+        })
+        // テキストエリアの削除
+        clipboard.remove();
+        // アラート文の表示
+        alert('クリップボードにコピーしました');
+    });
+})();
